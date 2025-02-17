489edd471dabc6b7ab5111b67f984b308322a2df
zayd <zaydthirteen@gmail.com>
2025-02-17 10:22:39 +0530

    Replaced bcrypt with bcryptjs

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const userModel = require("../models/userModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const multer = require("multer");
 const path = require("path");
 
