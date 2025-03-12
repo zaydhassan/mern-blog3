8c4816b2c11a36ca8078c4fc686d878b568e204f
Zayd Hassan <93902571+zaydhassan@users.noreply.github.com>
2025-03-12 10:55:40 +0530

    Update firebaseConfig.js

--- client/src/firebase/firebaseConfig.js
+++ client/src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';  
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyC1774CVhgVUQqXe9fXS1-yPvbLO6gC28I",
+    authDomain: "blog-website-806fd.firebaseapp.com",
+    projectId: "blog-website-806fd",
+    storageBucket: "blog-website-806fd.appspot.com",
+    messagingSenderId: "493205175438",
+    appId: "1:493205175438:web:e2dcbcff28d3428e4adddf"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
